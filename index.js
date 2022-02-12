3815dd885a1c6b6c1e82db95808a51e9884f1f9b
Christopher Sadler <sadlerlogic@gmail.com>
2022-02-11 19:59:49 -0600

    commit tree 25a528220f43e19c32ee1729818c2d785cbe4b93

--- index.js
+++ index.js
@@ -1,2 +1,18 @@
+let num1 = 31;
+let num2 = 2;
+let multiply = num1 * num2;
 
-// Write your code here
+let random;
+ random = Math.floor(Math.random() * 10);
+
+Number.isInteger(random);
+
+let mod;
+let num3
+let num4
+num3 = 19;
+num4 = 5;
+mod = num3 % num4;
+
+let max;
+max = Math.max(5, 10, 15, 20);
